9551e3b9990a95eb6588165f94e33af93e31571e
Scott Briggs <hopingforclearskies@gmail.com>
2022-12-06 21:33:26 -0600

    Added functions for deltaT and coordinate transforms

--- src/R/coordinate.R
+++ src/R/coordinate.R
@@ -29,3 +29,78 @@ pos2RaDec <- function(pos){
   
   return (c(m_r, m_phi, m_theta))
 }
+
+# Returns a rotation matrix based on the axis (x, y, or z) and the angle phi
+# The angle phi is expected to be in radians
+rotationMatrix <- function(axis, phi)
+{
+  mat <- matrix(0.0, nrow = 3, ncol = 3)
+  cosphi <- cos(phi)
+  sinphi <- sin(phi)
+  
+  if (axis == 1) {
+    mat[1,1] <- 1.0
+    mat[2,2] <- cosphi
+    mat[3,3] <- cosphi
+    mat[2,3] <- sinphi
+    mat[3,2] <- -sinphi
+  } else if (axis == 2) {
+    mat[1,1] <- cosphi
+    mat[1,3] <- -sinphi
+    mat[2,2] <- 1.0
+    mat[3,1] <- sinphi
+    mat[3,3] <- cosphi
+  } else if (axis == 3){
+    mat[1,1] <- cosphi
+    mat[2,2] <- cosphi
+    mat[3,3] <- 1.0
+    mat[2,1] <- -sinphi
+    mat[1,2] <- sinphi
+  } else {print("Axis is wrong value")}
+  
+  return (mat)
+}
+
+# Convert an equatorial position vector to an ecliptical position vector
+# Obliquity is in radians
+equatorialToEcliptical <- function(pos, obliquity)
+{
+  pos1 <- rotation_matrix(1, obliquity) %*% pos
+  
+  return (pos1)
+}
+
+# Convert an ecliptical position vector to an equatorial position vector
+# Obliquity is in radians
+eclipticalToEquatorial <- function(pos, obliquity)
+{
+  pos1 <- rotation_matrix(1, -obliquity) %*% pos
+  
+  return(pos1)
+}
+
+# Convert equatorial coordinates (hour angle, declination) in radians to
+# horizon coordinates (azimuth, altitude) in radians.
+equatorialToHorizon <- function(hour_angle, dec, obs_lat)
+{
+  vec1 <- c(hour_angle, dec, 1)
+  vec2 <- rotation_matrix(2, (PI/2 - obs_lat)) %*% vec1
+  
+  azimuth <- vec2[1]
+  altitude <- vec2[2]
+  
+  return(c(azimuth, altitude))
+}
+
+# Convert horizon coordinates (azimuth, altitude) in radians to
+# equatorial coordinates (hour angle, declination) in radians.
+horizonToEquatorial <- function(azimuth, altitude, obs_lat)
+{
+  vec1 <- c(azimuth, altitude, 1)
+  vec2 <- rotation_matrix(2, -(PI/2 - obs_lat)) %*% vec1
+  
+  hour_angle <- vec2[1]
+  dec <- vec2[2]
+  
+  return(c(hour_angle, dec))
+}
